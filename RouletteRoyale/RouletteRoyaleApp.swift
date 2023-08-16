a06d8fde17e2a9c29c77cc58405f3ed7659a7efb
iosdev54 <iosdev54@gmail.com>
2023-08-16 11:03:33 +0300

    Initial Commit

---
+++ RouletteRoyale/RouletteRoyaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteRoyaleApp.swift
+//  RouletteRoyale
+//
+//  Created by Dmytro Grytsenko on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RouletteRoyaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
